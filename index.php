fca2373569c7e1c3dceaecf6f5b739a763d006b3
unknown <augusto.milao01@gmail.com>
2022-02-13 01:49:25 -0300

    index + css

--- index.php
+++ index.php
@@ -5,19 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="style.css">
-    <title>Avalie meu Professor</title>
+    <title>Avalie Seu Professor</title>
 </head>
 <body>
     <div class="nav">
-        <h3 class="titulo principal">Avalie Meu Professor</h3>
+        <h3 class="titulo principal bordatexto">Avalie Seu Professor</h3>
         <button class="expandir principal">Votar</button>
     </div>
-    <div class="explicacao centro">
-        <h1 class="principal centro">Nosso Objetivo</h1>
-        <h3 class="principal centro objetivo">Após perceber que a maioria dos alunos faziam uma lista dos professores favoritos para cada matéria que ainda seriam feitas, vimos que poderiamos ajudar eles nessa tarefa, juntando informações de seus alunos anteriores.</h3>
+    <div class="pesquisa centro">
+        <h1 class="centro principal bordatexto">Procure o Professor</h1> <br>
+        <input>
+
     </div>
     <div class="divisao preto centro">
         <h2 class="respeito">Sejam Respeitosos!</h2>
+    </div>
+    <div class="explicacao centro">
+        <h1 class="principal centro bordatexto">Nosso Objetivo</h1><br>
+        <h3 class="principal centro objetivo">Após perceber que a maioria dos alunos faziam uma lista dos professores favoritos para cada matéria que ainda seriam feitas, vimos que poderiamos ajudar eles nessa tarefa, juntando informações de seus alunos anteriores.</h3>
     </div>
 </body>
 </html>
