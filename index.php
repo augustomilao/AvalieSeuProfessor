40782284578721e476e7da8a7d7078580a04bd57
unknown <augusto.milao01@gmail.com>
2022-02-13 01:05:29 -0300

    index + css

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>Avalie meu Professor</title>
+</head>
+<body>
+    <div class="nav">
+        <h3 class="titulo principal">Avalie Meu Professor</h3>
+        <button class="expandir principal">Votar</button>
+    </div>
+    <div class="explicacao centro">
+        <h1 class="principal centro">Nosso Objetivo</h1>
+        <h3 class="principal centro objetivo">Após perceber que a maioria dos alunos faziam uma lista dos professores favoritos para cada matéria que ainda seriam feitas, vimos que poderiamos ajudar eles nessa tarefa, juntando informações de seus alunos anteriores.</h3>
+    </div>
+    <div class="divisao preto centro">
+        <h2 class="respeito">Sejam Respeitosos!</h2>
+    </div>
+</body>
+</html>
